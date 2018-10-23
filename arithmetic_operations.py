71adcd0b7d64be04fae833e709129597447a0faf
sne007 <challasnehith@gmail.com>
2018-10-23 18:58:15 -0400

    Initial commit

---
+++ arithmetic_operations.py
@@ -0,0 +1,24 @@
+#author: snehith reddy challa
+#python basic arithmetic operations
+
+#basic addition
+print("basic addition 2+2 = " + str(2+2))
+#basic subtraction
+print("basic subtraction 4-1 = " + str(4-1))
+
+
+#basic multiplication
+print("basic multiplication 2*10 = " + str(2*10))
+#multiplication of int and strings
+print(5* "snehith ")
+#multiplication by the number itself. Also known as exponentiation can be done with '**'
+print(8**3)
+#strings cant be multiplied with strings. and also multiplication cant be done with floats.
+
+
+#basic division, but prints a float(or a double) value
+print(8/3)
+#integer division
+print(8//3)
+
+
