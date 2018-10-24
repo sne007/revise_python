2f639227092b0ff9930154f0d6a9fe7922784916
sne007 <challasnehith@gmail.com>
2018-10-23 20:27:33 -0400

    test variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+print("input name")
+name = input()
+print("input age")
+age = input()
+print("input gender")
+gender = input()
+
+#print all the values scanned
+print(" name is " + name + " " + " age is " + age + " " + " and gender is " + gender + "\n")
+
+#len() is used for getting length
+print('you have ' + str(len((name))) + ' characters in your name lol')
+
